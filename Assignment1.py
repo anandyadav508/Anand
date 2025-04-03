8f115085a10b7ee54f730db5e73763bd046fd79a
Anand Kumar Yadav <123584705+anandyadav508@users.noreply.github.com>
2025-04-03 20:20:20 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,23 @@
+'''1'''
+'''
+a=float(input('enter the first number : '))
+b=float(input('enter the second number : '))
+
+x=a+b
+y=a-b
+z=a*b
+d=a/b
+print ('Addition: ',x)
+print ('Subtraction: ',y)
+print ('Multiplication: ',z)
+print ('Division: ',d)
+'''
+
+
+
+'''2'''
+a=str(input('enter the first name : '))
+b=str(input('enter the second name : '))
+
+x=a+' '+b+'!'
+print('hello,',x,'welcome to python program')
